cfd9d6200efdd87102f89832b8916e2f7a15faae
Afens <sheith.92@gmail.com>
2015-10-14 14:11:15 +0200

    MiniP2

---
+++ MiniPractica2/MiniPractica2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace MiniPractica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Crear = new System.Windows.Forms.Timer(this.components);
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.Desmarcar = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(96, 148);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Crear
+            // 
+            this.Crear.Enabled = true;
+            this.Crear.Tick += new System.EventHandler(this.Crear_Tick);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 32);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Desmarcar
+            // 
+            this.Desmarcar.Tick += new System.EventHandler(this.Desmarcar_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer Crear;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer Desmarcar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MiniPractica2/MiniPractica2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniPractica2
+{
+    public partial class Form1 : Form
+    {
+        Point anterior = new Point(12, -8);
+        int a = 20;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Crear_Tick(object sender, EventArgs e)
+        {
+            int x=anterior.X, y=anterior.Y;
+            label1.Text = x.ToString();
+            label2.Text = y.ToString();
+            Point posicion;
+            if(x>this.Size.Width-15 && y > this.Size.Height - 15)
+            {
+                Crear.Enabled = false;
+            }
+            else
+            {
+                if (y + 20 > this.Size.Height - 15)
+                {
+                    posicion = new Point(x + 20, y);
+                }
+                else
+                {
+                    posicion = new Point(x , y + 20);
+                }
+                CheckBox cb = new CheckBox();
+                Controls.Add(cb);
+                cb.Location = posicion;
+                anterior = posicion;
+            }
+            
+            
+
+        }
+
+        private void Desmarcar_Tick(object sender, EventArgs e)
+        {
+            foreach (Object x in Controls)
+            {
+                if (x is CheckBox)
+                    ((CheckBox)x).Checked = false;
+            }
+        }
+    }
+}
